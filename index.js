c4d3132c23d8f9d9167dccc646e36eea1acb1d7d
Daniel Choudhury <dannychoudhury@gmail.com>
2020-07-30 14:59:03 +0100

    Added buildEvent input to customise when the plugin runs

--- index.js
+++ index.js
@@ -1,89 +1,97 @@
-const fs = require("fs");
-const path = require("path");
-const babel = require("@babel/core");
-const inlinePlugin = require("babel-plugin-transform-inline-environment-variables");
+const fs = require('fs')
+const path = require('path')
+
+const babel = require('@babel/core')
+const inlinePlugin = require('babel-plugin-transform-inline-environment-variables')
 
 async function findAllJSFiles(folder) {
-  const names = await fs.promises.readdir(folder);
+  const names = await fs.promises.readdir(folder)
 
   const jsFilePaths = await Promise.all(
-    names.map(async name => {
-      const absPath = path.join(folder, name);
+    names.map(async (name) => {
+      const absPath = path.join(folder, name)
 
-      const f = await fs.promises.stat(absPath);
+      const f = await fs.promises.stat(absPath)
 
       if (f.isFile()) {
-        if (!name.endsWith(".js")) {
-          return null;
+        if (!name.endsWith('.js')) {
+          return null
         }
 
-        return absPath;
+        return absPath
       } else if (f.isDirectory()) {
         // ignore node_modules
-        if (name === "node_modules") {
-          return null;
+        if (name === 'node_modules') {
+          return null
         }
 
-        return findAllJSFiles(absPath);
+        return findAllJSFiles(absPath)
       } else {
         // ignore symlinks and others
-        return null;
+        return null
       }
     })
-  );
+  )
 
-  return jsFilePaths.flat().filter(p => !!p);
+  return jsFilePaths.flat().filter((p) => !!p)
 }
 
 async function inlineEnv(path, verbose = false) {
-  console.log("inlining", path);
+  console.log('inlining', path)
 
   const transformed = await babel.transformFileAsync(path, {
     plugins: [babel.createConfigItem(inlinePlugin)],
-    retainLines: true
-  });
+    retainLines: true,
+  })
 
   if (verbose) {
-    console.log("transformed code", transformed.code);
+    console.log('transformed code', transformed.code)
   }
 
-  await fs.promises.writeFile(path, transformed.code, "utf8");
+  await fs.promises.writeFile(path, transformed.code, 'utf8')
 }
 
-module.exports = {
-  onPreBuild: async ({ inputs, utils, netlifyConfig }) => {
-    const verbose = !!inputs.verbose;
+async function processFiles({ inputs, utils, netlifyConfig }) {
+  const verbose = !!inputs.verbose
 
-    if (verbose) {
-      console.log(
-        "build env contains the following environment variables",
-        Object.keys(process.env)
-      );
-    }
-
-    if (netlifyConfig.build && netlifyConfig.build.functions) {
-      try {
-        const files = await findAllJSFiles(netlifyConfig.build.functions);
-
-        if (verbose) {
-          console.log("found function files", files);
-        }
-
-        await Promise.all(files.map(f => inlineEnv(f, verbose)));
-
-        utils.status.show({
-          summary: `Processed ${files.length} function file(s).`
-        });
-      } catch (err) {
-        return utils.build.failBuild(
-          `Failed to inline function files due to the following error:\n${err.message}`,
-          { error: err }
-        );
-      }
-    } else {
-      utils.status.show({
-        summary: "Skipped processing because the project had no functions."
-      });
-    }
+  if (verbose) {
+    console.log(
+      'build env contains the following environment variables',
+      Object.keys(process.env)
+    )
   }
-};
+
+  if (netlifyConfig.build && netlifyConfig.build.functions) {
+    try {
+      const files = await findAllJSFiles(netlifyConfig.build.functions)
+
+      if (verbose) {
+        console.log('found function files', files)
+      }
+
+      await Promise.all(files.map((f) => inlineEnv(f, verbose)))
+
+      utils.status.show({
+        summary: `Processed ${files.length} function file(s).`,
+      })
+    } catch (err) {
+      return utils.build.failBuild(
+        `Failed to inline function files due to the following error:\n${err.message}`,
+        { error: err }
+      )
+    }
+  } else {
+    utils.status.show({
+      summary: 'Skipped processing because the project had no functions.',
+    })
+  }
+}
+
+module.exports = (inputs) => {
+  // Use user configured buildEvent
+  const buildEvent = inputs.buildEvent || 'onPreBuild'
+
+  return {
+    [buildEvent]: processFiles,
+  }
+}
